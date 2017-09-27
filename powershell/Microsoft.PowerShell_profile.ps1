fe63b8d78f4c8ce44b34378b83ce6698bc409d0a
John Knoll <jpknoll@gmail.com>
2017-09-27 10:48:56 -0700

    add some more tools

--- powershell/Microsoft.PowerShell_profile.ps1
+++ powershell/Microsoft.PowerShell_profile.ps1
@@ -2,10 +2,13 @@ if (-not (Get-Command git)) {
   $env:Path += ";C:\Program Files\Git\bin"
 }
 
-$env:Path += ";C:\Users\knoll\AppData\Roaming\npm"
+$env:Path += ";C:\Users\jpknoll\AppData\Roaming\npm"
 
-$env:Path += ";C:\Users\knoll\.dnx\bin"
-dnvm use default
+$env:Path += ";C:\Users\jpknoll\.dnx\bin"
+
+$env:Path += ";C:\Users\jpknoll\AppData\Local\Android\sdk\platform-tools"
+
+$env:Path += ";C:\Program Files (x86)\Microsoft SDKs\Azure\Storage Emulator"
 
 function which($name)
 {
